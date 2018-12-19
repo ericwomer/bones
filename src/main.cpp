7ad04fe4ca738675fd62d7b564c33546dc170404
Eric <ewomer@hotmail.com>
2018-12-18 22:02:12 -0500

    Ripped out the cruft and crated a bare bones meson project for simple testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,5 @@ using std::vector;
 
 int main(int argc, char* argv[])
 {
-  // Application class that calls main
-  unique_ptr<Base::Application> application = make_unique<Skeleton>();
-
-  // Convert c style list of strings to a c++ vector of strings
-  vector<string> params;
-  for (int i = 0; i != argc; ++i) {
-    params.push_back(argv[i]);
-  }
-
-  try {
-    application->main(params);
-  } catch (const exception& e) {
-    cerr << e.what() << '\n';
-    return EXIT_FAILURE;
-  }
-
   return EXIT_SUCCESS;
 }
