c54f9dee6603fdcefa7dfb6f23859900cb45c28a
ewomer <ewomer@hotmail.com>
2026-02-12 21:07:42 -0500

    Use println instead of cout

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 // 
-
-#include <iostream>
 #include <vector>
 #include <string_view>
 #include <iterator>
 #include <cstddef>
 #include <cstdlib>
+#include <print>
 
 using std::vector;
 using std::next;
 using std::ptrdiff_t;
 using std::string_view;
+using std::println;
 
-// Jason Turner better main
+// Jason Turner's better main
 auto main( const int argc, const char * const * argv) -> int
 {
   // argv are converted to string_views args
   vector<string_view> args(argv, next(argv, static_cast<ptrdiff_t>(argc)));
 
   // Do Work Here
-  std::cout << "Hello,World!\n";
+  println("Hello,World!");
   
   return EXIT_SUCCESS;
 }
