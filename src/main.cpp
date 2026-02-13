20b3ae7ce2c85c630b9d159152291fafd56690fe
ewomer <ewomer@hotmail.com>
2026-02-12 19:52:06 -0500

    Several updates including chooseing to to GNU free or not

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
-/**
- * \file main.cpp
- * \brief Vector bubble sort example
- */
+// 
 
-#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <string_view>
+#include <iterator>
+#include <cstddef>
+#include <cstdlib>
 
-/**
- * \brief Main entry point
- * \param argc
- * \param argv
- * \return 0 if all goes well
- */
-int main( [[maybe_unused]] int argc, [[maybe_unused]] char *argv[], [[ maybe_unused]] char *envp[])
+using std::vector;
+using std::next;
+using std::ptrdiff_t;
+using std::string_view;
+
+// Jason Turner better main
+auto main( const int argc, const char * const * argv) -> int
 {
+  // argv are converted to string_views args
+  vector<string_view> args(argv, next(argv, static_cast<ptrdiff_t>(argc)));
+
   // Do Work Here
   std::cout << "Hello,World!\n";
   
